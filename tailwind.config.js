c470b6a67dccd582f6ddb07eaf360a2f647949b0
umbat <farhanpratamax@gmail.com>
2023-01-21 20:24:30 +0700

    change the sidenav to tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./index.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins'],
+      },
+    },
   },
   plugins: [],
-}
+};

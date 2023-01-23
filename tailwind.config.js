563e7d7c1773f9324d8f8cc59a1c7e453bc43c57
umbat <farhanpratamax@gmail.com>
2023-01-23 14:25:38 +0700

    success call the map in html yet, not in js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,20 @@ module.exports = {
       fontFamily: {
         poppins: ['Poppins'],
       },
+      colors: {
+        'hijau-tua-banget': '#0B382F',
+        'hijau-tua': '#1C6758',
+        'hijau-muda': '#3D8361',
+        gading: '#D6CDA4',
+        'putih-kuning': '#EEF2E6',
+      },
+      spacing: {
+        '230px': '230px',
+        '640px': '640px',
+        '420px': '420px',
+        '480px': '480px',
+        '1080px': '1080px',
+      },
     },
   },
   plugins: [],
